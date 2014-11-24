32b6287f08d39a20d86b3671bc3e1f1e724e4ff7
Greta Zhang <greta.zhang@mediatek.com>
2014-11-24 14:12:52 +0800

    let wdt output reset signal when reboot signal comes
    modify code style, reboot callback function
    local test pass
    
    Change-Id: Icf790af86746677b55e7e1cd4ee1604a7232413c

--- drivers/watchdog/mt65xx_wdt.c
+++ drivers/watchdog/mt65xx_wdt.c
@@ -30,27 +30,34 @@
 #include <linux/of_address.h>
 #include <linux/of_irq.h>
 #include <linux/notifier.h>
+#include <linux/delay.h>
 
 #define WATCHDOG_NAME          "Mt65xx_Watchdog"
 
 /*WDT_MODE*/
-#define MT65XX_WDT_MODE        (0x0000)
-#define MT65XX_WDT_MODE_KEY    (0x22000000)
-#define MT65XX_WDT_MODE_ENABLE (0x25)
+#define MT65XX_WDT_MODE        0x00
+#define MT65XX_WDT_MODE_KEY    0x22000000
+#define MT65XX_WDT_MODE_AUTO_START (1 << 4)
+#define MT65XX_WDT_MODE_EXTEN (1 << 2)
+#define MT65XX_WDT_MODE_EN (1 << 0)
 
-/*WDT_LENGTH
+/*
+* WDT_LENGTH
 * Key = BitField  4:0 = 0x08
 * TimeOut = BitField 15:5
 * 1 tick means 512 * T32.768K = 15.6 ms --> 1s = 1000/15.6  = 64 ticks
 */
-#define MT65XX_WDT_LENGTH      (0x0004)
-#define MT65XX_WDT_LENGTH_KEY  (0x0008)
+#define MT65XX_WDT_LENGTH      0x04
+#define MT65XX_WDT_LENGTH_KEY  0x08
 #define MT65XX_WDT_LENGTH_FIELD_OFFSET  5
 #define MT65XX_WDT_LENGTH_SECOND_TO_TICKS  64
 
 /*WDT_RESTART*/
-#define MT65XX_WDT_RESTART     (0x0008)
-#define MT65XX_WDT_RESTART_KEY (0x1971)
+#define MT65XX_WDT_RESTART     0x08
+#define MT65XX_WDT_RESTART_KEY 0x1971
+
+#define MT65XX_WDT_SWRST		0x14
+#define MT65XX_WDT_SWRST_KEY		0x1209
 
 /* in seconds */
 #define MT65XX_MIN_TIMEOUT     1
@@ -59,8 +66,8 @@
 
 /**
  * struct mt65xx_wdt: mt65xx wdt device structure
- * @wdt_device: instance of struct watchdog_device
- * @lock: spin lock protecting dev structure and io access
+ * @wdd: instance of struct watchdog_device
+ * @wdt_notifier: notifier of system down and system halt
  * @reg_base: base address of wdt
  * @timeout: current programmed timeout
  */
@@ -78,10 +85,13 @@ MODULE_PARM_DESC(nowayout, "Watchdog cannot be stopped once started (default="
 
 static int wdt_enable(struct watchdog_device *wdd)
 {
-	struct mt65xx_wdt *wdt = watchdog_get_drvdata(wdd);
 	unsigned int tmp;
+	struct mt65xx_wdt *wdt = watchdog_get_drvdata(wdd);
 
-	tmp = MT65XX_WDT_MODE_KEY | MT65XX_WDT_MODE_ENABLE;
+	wdt->wdd.ops->set_timeout(&wdt->wdd, wdt->wdd.timeout);
+
+	tmp = MT65XX_WDT_MODE_KEY | MT65XX_WDT_MODE_AUTO_START
+		    | MT65XX_WDT_MODE_EXTEN | MT65XX_WDT_MODE_EN;
 	writel(tmp, wdt->reg_base + MT65XX_WDT_MODE);
 
 	return 0;
@@ -92,7 +102,9 @@ static int wdt_disable(struct watchdog_device *wdd)
 	struct mt65xx_wdt *wdt = watchdog_get_drvdata(wdd);
 	unsigned int tmp;
 
-	tmp = MT65XX_WDT_MODE_KEY;
+	tmp = readl(wdt->reg_base + MT65XX_WDT_MODE);
+	tmp |= MT65XX_WDT_MODE_KEY;
+	tmp &= ~(MT65XX_WDT_MODE_EN);
 	writel(tmp, wdt->reg_base + MT65XX_WDT_MODE);
 
 	return 0;
@@ -102,7 +114,7 @@ static int wdt_ping(struct watchdog_device *wdd)
 {
 	struct mt65xx_wdt *wdt = watchdog_get_drvdata(wdd);
 
-	writel(MT65XX_WDT_RESTART_KEY, wdt->reg_base+MT65XX_WDT_RESTART);
+	writel(MT65XX_WDT_RESTART_KEY, wdt->reg_base + MT65XX_WDT_RESTART);
 
 	return 0;
 }
@@ -137,9 +149,21 @@ static int wdt_notify_sys(struct notifier_block *this, unsigned long code,
 {
 	struct mt65xx_wdt *wdt = container_of(this, struct mt65xx_wdt,
 								wdt_notifier);
+	unsigned int tmp;
 
-	if (code == SYS_DOWN || code == SYS_HALT)
-		wdt_disable(&wdt->wdd);
+	if (code == SYS_DOWN || code == SYS_HALT) {
+		tmp = readl(wdt->reg_base + MT65XX_WDT_MODE);
+		tmp |= MT65XX_WDT_MODE_KEY;
+		tmp |= MT65XX_WDT_MODE_AUTO_START | MT65XX_WDT_MODE_EXTEN;
+		writel(tmp, wdt->reg_base + MT65XX_WDT_MODE);
+
+		writel(MT65XX_WDT_SWRST_KEY, wdt->reg_base + MT65XX_WDT_SWRST);
+	}
+
+	while (1) {
+		mdelay(5);
+		writel(MT65XX_WDT_SWRST_KEY, wdt->reg_base + MT65XX_WDT_SWRST);
+	}
 
 	return NOTIFY_DONE;
 }
@@ -157,14 +181,14 @@ static int mt65xx_wdt_probe(struct platform_device *pdev)
 	int rc;
 
 	wdt = devm_kzalloc(&pdev->dev, sizeof(*wdt), GFP_KERNEL);
-	if (NULL == wdt)
+	if (!wdt)
 		return -ENOMEM;
 
 	/* initialize mt65xx_wdt structure */
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	wdt->reg_base = devm_ioremap_resource(&pdev->dev, res);
 	if (NULL == wdt->reg_base) {
-		dev_err(&pdev->dev, "wdt->reg_base== NULL\n");
+		dev_err(&pdev->dev, "wdt->reg_base == NULL\n");
 		ret = -ENOMEM;
 	}
 
@@ -178,18 +202,20 @@ static int mt65xx_wdt_probe(struct platform_device *pdev)
 	watchdog_set_drvdata(&wdt->wdd, wdt);
 
 	ret = watchdog_register_device(&wdt->wdd);
-	if (ret)
+	if (ret) {
 		dev_err(&pdev->dev, "watchdog_register_device() failed: %d\n",
 				ret);
-
+		return ret;
+	}
 	platform_set_drvdata(pdev, wdt);
 	wdt_disable(&wdt->wdd);
 
 	wdt->wdt_notifier.notifier_call = wdt_notify_sys;
 	rc = register_reboot_notifier(&wdt->wdt_notifier);
-	if (rc)
-		dev_err("cannot register reboot notifier (err=%d)\n", rc);
-
+	if (rc) {
+		unregister_reboot_notifier(&wdt->wdt_notifier);
+		dev_err("cannot register reboot notifier (err = %d)\n", rc);
+	}
 	return ret;
 }
 
@@ -197,11 +223,8 @@ static int mt65xx_wdt_remove(struct platform_device *pdev)
 {
 	struct mt65xx_wdt *wdt = platform_get_drvdata(pdev);
 
-	if (wdt) {
-		platform_set_drvdata(pdev, NULL);
-		iounmap(wdt->reg_base);
-		kfree(wdt);
-	}
+	platform_set_drvdata(pdev, NULL);
+
 	unregister_reboot_notifier(&wdt->wdt_notifier);
 	watchdog_unregister_device(&wdt->wdd);
 
