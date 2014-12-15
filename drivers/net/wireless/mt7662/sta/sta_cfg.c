87e9aa8e80604127e9af61232659f3abdd23036c
George Kuo <george.kuo@mediatek.com>
2014-12-15 14:55:53 +0800

    remove reference to __DATE__ and __TIME__ for gcc 4.9 and "-Werror=date-time". Besides, none of them is required for source release now.
    Change-Id: I3a8f902388bf6e1f503af111141ebb330f74a842

--- drivers/net/wireless/mt7662/sta/sta_cfg.c
+++ drivers/net/wireless/mt7662/sta/sta_cfg.c
@@ -11255,9 +11255,10 @@ VOID RTMPIoctlShow(
             wrq->u.data.length = strlen(extra) + 1; /* 1: size of '\0' */
             break;
         case SHOW_DRVIER_VERION:
-            snprintf(extra, size, "Driver version-%s, %s %s\n", STA_DRIVER_VERSION, __DATE__, __TIME__ );
+		snprintf(extra, size, "Driver version-%s\n",
+			STA_DRIVER_VERSION);
 #ifdef MESH_SUPPORT
-	     			sprintf(extra+strlen(extra), "Support MESH \n");
+		sprintf(extra+strlen(extra), "Support MESH\n");
 #endif /* MESH_SUPPORT */
             wrq->u.data.length = strlen(extra) + 1; /* 1: size of '\0' */
             break;
