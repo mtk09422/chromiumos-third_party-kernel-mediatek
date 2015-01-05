d8e8fae3084689a928498a8a689863ef640368d4
Ben Lok <ben.lok@mediatek.com>
2015-01-05 20:35:13 +0800

    [Embedded Controller] Porting Google's EC patch for MT8173 SOC.
    1. Porting Google's cros_ec Linux kernel driver from v3.14 ToT branch.
    2. Ignore cros_ec_lightbar driver due to compiling error of arm64 toolchain.
    
    Signed-off-by: Ben Lok <ben.lok@mediatek.com>

--- include/linux/mfd/cros_ec.h
+++ include/linux/mfd/cros_ec.h
@@ -16,9 +16,30 @@
 #ifndef __LINUX_MFD_CROS_EC_H
 #define __LINUX_MFD_CROS_EC_H
 
-#include <linux/notifier.h>
+#include <linux/cdev.h>
+#include <linux/power_supply.h>
 #include <linux/mfd/cros_ec_commands.h>
+#include <linux/mfd/cros_ec_dev.h>
 #include <linux/mutex.h>
+
+/*
+ * The EC is unresponsive for a time after a reboot command.  Add a
+ * simple delay to make sure that the bus stays locked.
+ */
+#define EC_REBOOT_DELAY_MS		50
+
+/*
+ * Max bus-specific overhead incurred by request/responses.
+ * I2C requires 1 additional byte for requests.
+ * I2C requires 2 additional bytes for responses.
+ * */
+#define EC_MAX_REQUEST_OVERHEAD		1
+#define EC_MAX_RESPONSE_OVERHEAD	2
+
+#define EC_PROTO_VERSION_UNKNOWN	0
+
+/* ec_command return value for non-success result from EC */
+#define EECRESULT 1000
 
 /*
  * Command interface between EC and AP, for LPC, I2C and SPI interfaces.
@@ -35,33 +56,19 @@ enum {
 					EC_MSG_TX_PROTO_BYTES,
 };
 
-/*
- * @version: Command version number (often 0)
- * @command: Command to send (EC_CMD_...)
- * @outdata: Outgoing data to EC
- * @outsize: Outgoing length in bytes
- * @indata: Where to put the incoming data from EC
- * @insize: Max number of bytes to accept from EC
- * @result: EC's response to the command (separate from communication failure)
- */
-struct cros_ec_command {
-	uint32_t version;
-	uint32_t command;
-	uint8_t *outdata;
-	uint32_t outsize;
-	uint8_t *indata;
-	uint32_t insize;
-	uint32_t result;
-};
-
 /**
  * struct cros_ec_device - Information about a ChromeOS EC device
  *
- * @ec_name: name of EC device (e.g. 'chromeos-ec')
  * @phys_name: name of physical comms layer (e.g. 'i2c-4')
- * @dev: Device pointer
+ * @dev: Device pointer for physical comms device
  * @was_wake_device: true if this device was set to wake the system from
  * sleep at the last suspend
+ * @cmd_read_mem: direct read of the EC memory-mapped region, if supported
+ *     @offset is within EC_LPC_ADDR_MEMMAP region.
+ *     @bytes: number of bytes to read. zero means "read a string" (including
+ *     the trailing '\0'). At most only EC_MEMMAP_SIZE bytes can be read.
+ *     Caller must ensure that the buffer is large enough for the result when
+ *     reading a string.
  *
  * @priv: Private data
  * @irq: Interrupt to use
@@ -76,35 +83,47 @@ struct cros_ec_command {
  * to using dword.
  * @din_size: size of din buffer to allocate (zero to use static din)
  * @dout_size: size of dout buffer to allocate (zero to use static dout)
- * @parent: pointer to parent device (e.g. i2c or spi device)
  * @wake_enabled: true if this device can wake the system from sleep
+ * @lock: one transaction at a time
  * @cmd_xfer: send command to EC and get response
  *     Returns the number of bytes received if the communication succeeded, but
  *     that doesn't mean the EC was happy with the command. The caller
  *     should check msg.result for the EC's result code.
- * @lock: one transaction at a time
  */
 struct cros_ec_device {
 
 	/* These are used by other drivers that want to talk to the EC */
-	const char *ec_name;
 	const char *phys_name;
 	struct device *dev;
 	bool was_wake_device;
 	struct class *cros_class;
+	int (*cmd_readmem)(struct cros_ec_device *ec, unsigned int offset,
+			   unsigned int bytes, void *dest);
+	int (*cmd_read_u32)(struct cros_ec_device *ec, unsigned int offset,
+			    u32 *dest);
+	int (*cmd_read_u16)(struct cros_ec_device *ec, unsigned int offset,
+			    u16 *dest);
+	int (*cmd_read_u8)(struct cros_ec_device *ec, unsigned int offset,
+			   u8 *dest);
 
 	/* These are used to implement the platform-specific interface */
+	u16 max_request;
+	u16 max_response;
+	u16 max_passthru;
+	u16 proto_version;
 	void *priv;
 	int irq;
-	uint8_t *din;
-	uint8_t *dout;
+	u8 *din;
+	u8 *dout;
 	int din_size;
 	int dout_size;
-	struct device *parent;
 	bool wake_enabled;
+	struct mutex lock;
 	int (*cmd_xfer)(struct cros_ec_device *ec,
 			struct cros_ec_command *msg);
-	struct mutex lock;
+	int (*pkt_xfer)(struct cros_ec_device *ec,
+			struct cros_ec_command *msg);
+	struct power_supply *charger;
 };
 
 /**
@@ -139,18 +158,6 @@ int cros_ec_resume(struct cros_ec_device *ec_dev);
  */
 int cros_ec_prepare_tx(struct cros_ec_device *ec_dev,
 		       struct cros_ec_command *msg);
-
-/**
- * cros_ec_check_result - Check ec_msg->result
- *
- * This is used by ChromeOS EC drivers to check the ec_msg->result for
- * errors and to warn about them.
- *
- * @ec_dev: EC device
- * @msg: Message to check
- */
-int cros_ec_check_result(struct cros_ec_device *ec_dev,
-			 struct cros_ec_command *msg);
 
 /**
  * cros_ec_cmd_xfer - Send a command to the ChromeOS EC
